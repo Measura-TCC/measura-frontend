26d2a3e0fbaca4e7f790454db60964780e81bb85
joaovictor-ferreira <joaovictor2111@gmail.com>
2025-11-29 14:17:49 -0300

    hotfix: sidebar order

--- src/presentation/assets/icons/ClipboardIcon.tsx
+++ src/presentation/assets/icons/ClipboardIcon.tsx
@@ -9,13 +9,12 @@ export const ClipboardIcon = ({ className = "w-5 h-5" }: IconProps) => {
       fill="none"
       stroke="currentColor"
       viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth={2}
-        d="M9 5H7a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
       />
     </svg>
   );
